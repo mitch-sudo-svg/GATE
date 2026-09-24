b481a21fa89b84c8975108db8bbb788079fc407c
mitch-sudo-svg <moliveribarra@gmail.com>
2026-09-24 10:25:27 -0700

    Create properties.js

---
+++ js/properties.js
@@ -0,0 +1,118 @@
+const properties = [
+
+    {
+        id: 1,
+
+        title: "Casa en Los Lagos",
+
+        operation: "venta",
+
+        type: "casa",
+
+        zone: "los lagos",
+
+        price: 15750000,
+
+        image:
+            "images/properties/los-lagos.jpg",
+
+        bedrooms: 3,
+
+        bathrooms: 4,
+
+        construction: 458.03,
+
+        land: 780,
+
+        description:
+            "Residencia ubicada en Los Lagos, actualmente en obra gris y con posibilidad de adaptar los espacios al gusto del comprador."
+    },
+
+
+    {
+        id: 2,
+
+        title: "Departamento Torre KYO Upday",
+
+        operation: "renta",
+
+        type: "departamento",
+
+        zone: "zona poniente",
+
+        price: 25000,
+
+        image:
+            "images/properties/kyo.jpg",
+
+        bedrooms: 3,
+
+        bathrooms: 2,
+
+        construction: 83.60,
+
+        land: null,
+
+        description:
+            "Departamento totalmente amueblado en Torre KYO Upday, ubicado en la zona poniente de Hermosillo."
+    },
+
+
+    {
+        id: 3,
+
+        title: "Departamento Torre Tres Ríos",
+
+        operation: "venta",
+
+        type: "departamento",
+
+        zone: "lomas altas",
+
+        price: 4570000,
+
+        image:
+            "images/properties/tres-rios.jpg",
+
+        bedrooms: 2,
+
+        bathrooms: 2,
+
+        construction: 120,
+
+        land: null,
+
+        description:
+            "Departamento en Torre Tres Ríos con amenidades, seguridad y excelente ubicación."
+    },
+
+
+    {
+        id: 4,
+
+        title: "Bodega Las Amapolas",
+
+        operation: "renta",
+
+        type: "bodega",
+
+        zone: "centro",
+
+        price: 45000,
+
+        image:
+            "images/properties/amapolas.jpg",
+
+        bedrooms: null,
+
+        bathrooms: 1,
+
+        construction: 667.5,
+
+        land: 667.5,
+
+        description:
+            "Bodega con espacio para operaciones comerciales, oficina y estacionamiento."
+    }
+
+];
